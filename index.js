b6e9597006e486f70cd0c9c24fb859a8847cadd2
areebaasim01 <161856617+areebaasim01@users.noreply.github.com>
2024-04-12 13:49:42 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+"use strict";
+// 7.	Number Eight: Write addition, subtraction, multiplication, and division operations that each 
+// result in the number 8. 
+// Be sure to enclose your operations in print statements to see the results.
+Object.defineProperty(exports, "__esModule", { value: true });
+// addition
+let a = 4;
+let b = 4;
+console.log(a + b);
+// subtraction
+let a1 = 16;
+let b1 = 8;
+console.log(a1 - b1);
+//  multiplication
+let a2 = 2;
+let b2 = 4;
+console.log(a2 * b2);
+// division
+let a3 = 16;
+let b3 = 2;
+console.log(a3 / b3);
---
+++ index.ts
@@ -0,0 +1,39 @@
+// 7.	Number Eight: Write addition, subtraction, multiplication, and division operations that each 
+// result in the number 8. 
+// Be sure to enclose your operations in print statements to see the results.
+
+// addition
+
+let a: number = 4
+let b: number = 4
+
+console.log(a + b);
+
+// subtraction
+
+let a1: number = 16
+let b1: number =  8
+
+console.log(a1 - b1);
+
+
+//  multiplication
+
+
+let a2: number = 2
+let b2: number = 4
+
+console.log(a2 * b2);
+
+
+// division
+
+
+let a3: number = 16
+let b3: number = 2
+
+console.log(a3 / b3);
+
+
+
+
